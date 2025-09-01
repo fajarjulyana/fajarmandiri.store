9a7f4cd0cb1800f973032552bd27b0fd5bbe6453
Fajar Julyana <fajarjulyana1@gmail.com>
2025-09-01 15:30:48 +0700

    deleted

--- make.exe.sh
+++
@@ -1,6 +0,0 @@
-pyinstaller --noconsole --onefile --icon=icon.ico \
-  --add-data "templates;templates" \
-  --add-data "static;static" \
-  --add-data "fajarmandiri.db;." \
-  --add-data "icon.ico;." \
-  app.pyw
